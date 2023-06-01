a039aa9612c44abc67c9e647ffc06fc1eb4b394c
Javed Ansari <javeda@thirdrocktechkno.com>
2023-06-01 10:57:44 +0530

    Remove the Textfiled for meeting ID & Changed the join screen

--- lib/livestream/api_call.dart
+++ lib/livestream/api_call.dart
@@ -5,13 +5,17 @@ import 'package:http/http.dart' as http;
 const token =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIwODM4ODAwMS01YWU2LTRkZjItODRlMS03MTc0MTI0YjcyNjMiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY4NTUzNTI0NiwiZXhwIjoxNjg2MTQwMDQ2fQ.jlwN9dw7y3TrFbv8Owvki_OAieRQYqSZu1ch-1dF8tg';
 
+const roomId = '6qqg-8kmk-8igm'; //Room Id (meeting Id)
+
 // API call to create meeting
 Future<String> createMeeting() async {
   final http.Response httpResponse = await http.post(
     Uri.parse('https://api.videosdk.live/v2/rooms'),
-    headers: {'Authorization': token},
+    headers: {
+      'Authorization': token,
+    },
   );
 
-//Destructuring the roomId from the response
+  //Destructuring the roomId from the response
   return json.decode(httpResponse.body)['roomId'];
 }
--- lib/livestream/join_screen.dart
+++ lib/livestream/join_screen.dart
@@ -4,9 +4,9 @@ import 'ils_screen.dart';
 import 'package:videosdk/videosdk.dart';
 
 class JoinScreen extends StatelessWidget {
-  final _meetingIdController = TextEditingController(text: "6qqg-8kmk-8igm");
+  //final _meetingIdController = TextEditingController(text: "6qqg-8kmk-8igm");
 
-  JoinScreen({super.key});
+  const JoinScreen({super.key});
 
   void onCreateButtonPressed(BuildContext context) async {
     // call api to create meeting and navigate to MeetingScreen with meetingId,token
@@ -29,10 +29,9 @@ class JoinScreen extends StatelessWidget {
   void onJoinButtonPressed(BuildContext context, Mode mode) {
     // check meeting id is not null or invaild
     // if meeting id is vaild then navigate to MeetingScreen with meetingId,token
-    String meetingId = _meetingIdController.text;
+    String meetingId = roomId;
     var re = RegExp("\\w{4}\\-\\w{4}\\-\\w{4}");
     if (meetingId.isNotEmpty && re.hasMatch(meetingId)) {
-      _meetingIdController.clear();
       Navigator.of(context).push(
         MaterialPageRoute(
           builder: (context) => ILSScreen(
@@ -44,46 +43,67 @@ class JoinScreen extends StatelessWidget {
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("Please enter valid meeting id"),
+        content: Text("Meeting id is not valid"),
       ));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text('VideoSDK ILS QuickStart'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => onCreateButtonPressed(context),
-              child: const Text('Create Meeting'),
-            ),
-            const SizedBox(height: 40),
-            TextField(
-              style: const TextStyle(color: Colors.white),
-              decoration: const InputDecoration(
-                hintText: 'Enter Meeting Id',
-                border: OutlineInputBorder(),
-                hintStyle: TextStyle(color: Colors.white),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: const Text("LiveChat"),
+        ),
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ElevatedButton(
+              //   onPressed: () => onCreateButtonPressed(context),
+              //   child: const Text('Create Meeting'),
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      onJoinButtonPressed(context, Mode.CONFERENCE);
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 100,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/live.png',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      onJoinButtonPressed(context, Mode.VIEWER);
+                    },
+                    child: Container(
+                      height: 85,
+                      width: 120,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/join.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              controller: _meetingIdController,
-            ),
-            ElevatedButton(
-              onPressed: () => onJoinButtonPressed(context, Mode.CONFERENCE),
-              child: const Text('Join Meeting as Host'),
-            ),
-            ElevatedButton(
-              onPressed: () => onJoinButtonPressed(context, Mode.VIEWER),
-              child: const Text('Join Meeting as Viewer'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
